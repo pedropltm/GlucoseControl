76d8ba8b45237e7b6a4c5a76990aa06b0ad554c2
Pedro Menezes <pedropltm@gmail.com>
2022-02-01 13:53:08 -0300

    Registering the database context.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using GlucoseControl.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<GlucoseControlContext>(opt =>
+    opt.UseInMemoryDatabase("GlucoseControlList"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,6 +18,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
