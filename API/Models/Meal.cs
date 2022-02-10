b8639afbdd10057459b9eb12f4aa845e0ccc6a4d
Pedro Menezes <pedropltm@gmail.com>
2022-02-09 22:17:11 -0300

    Annotated with [BsonId] to make this property the document's primary key.
    Annotated with [BsonRepresentation(BsonType.ObjectId)] to allow passing the parameter as type string instead of an ObjectId structure. Mongo handles the conversion from string to ObjectId.

--- API/Models/Meal.cs
+++ API/Models/Meal.cs
@@ -1,7 +1,12 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace GlucoseControl.Models
 {
     public class Meal
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public long Id { get; set; }
         public DateTime DateTime { get; set; }
         public string? Description { get; set; }
