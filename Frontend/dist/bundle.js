0aa8e1f0c214f79d574eab9198383a2dfaae1360
Pedro Menezes <pedropltm@gmail.com>
2022-02-23 09:52:46 -0300

    Created bundle.js file.

---
+++ Frontend/dist/bundle.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _user$adrress;
+
+var user = {
+  name: 'Pedro'
+};
+console.log((_user$adrress = user.adrress) === null || _user$adrress === void 0 ? void 0 : _user$adrress.street);
