e9a35802f663a79c43dfb42b573bf88283b1f393
Pedro Menezes <pedropltm@gmail.com>
2022-02-24 07:44:48 -0300

    Creating webpack.js file.

---
+++ Frontend/webpack.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+    entry: path.resolve(__dirname,'src', 'index.jsx'), // "__dirname" busca o diretório onde está a instrução
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js','.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    }
+}
